39898aa18ce8008d2255f93d6844e85d4c704777
MSAdministrator <rickardja@live.com>
2017-07-04 14:30:39 -0500

    Added detailed help about using New-LogFileClass function

--- PSLogging/Public/New-LogFileClass.ps1
+++ PSLogging/Public/New-LogFileClass.ps1
@@ -2,25 +2,36 @@
 {
     <#
         .SYNOPSIS
-        Placeholder
-
+         To use the [LogFile] Class you need to either call the New-LogFileClass.ps1 or by loading the entire PSLogging class in the 'Class' folder. 
+        
         .DESCRIPTION
-        Placeholder
+         This function will create a new [LogFile] class object and return it to the calling scope.
 
         .PARAMETER LogFile
-        Placeholder
+         LogFile is the file path and log you want to create and begin logging to
 
         .INPUTS
-        Placeholder
+         System.String
+
+         Pass in a string value for the path to create a LogFile
 
         .OUTPUTS
-        Placeholder
+         [LogFile] 
 
-        .EXAMPLE
-        Placeholder
+         This function will return a [LogFile] class object
+
+        .EXAMPLE 1
+         C:\PS> $LogFile = New-LogFileClass -LogFile 'C:\path\to\log\file\log.log'
+
+         By calling the New-LogFileClass function directly, it will return a [LogFile] data type that can now be used to log to
+
+        .EXAMPLE 2
+         C:\PS> $LogFile = New-LogFileClass -LogFile 'C:\path\to\log\file\log.txt'
+
+         By calling the New-LogFileClass function directly, it will return a [LogFile] data type that can now be used to log to
 
         .NOTES
-        Placeholder
+         New-LogFileClass will return a class object that can be used to log to a specified Log File / text file
     #>
     
     [CmdletBinding()]
